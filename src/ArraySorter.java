82826249f6156f92b58ebf2cf9ccbdce410f8024
okamodgi <nadakupnada@gmail.com>
2023-06-22 17:40:04 +0300

    laboratornaya16

--- src/ArraySorter.java
+++ src/ArraySorter.java
@@ -2,12 +2,15 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
+interface SortLogic {
+    int sortLog(int a, int b);
+}
 public class ArraySorter {
 
-    public static void sortArray(int[] array, Comparator<Integer> comparator) {
+    public static void sortArray(int[] array, SortLogic sortLogic) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
-                if (comparator.compare(array[j], array[j + 1]) > 0) {
+                if (sortLogic.sortLog(array[j], array[j + 1]) > 0) {
                     int swap = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = swap;
@@ -34,5 +37,4 @@ public class ArraySorter {
         sortArray(array, (a, b) -> b-a);
         System.out.println("Отсортированный массив по убыванию: " + Arrays.toString(array));
     }
-
 }
